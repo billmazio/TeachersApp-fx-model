c3d81f686fe606963a730e8627491f0b0fe9ef12
billmazio <bmaziotis@windowslive.com>
2022-11-23 17:00:20 +0200

    MainWindowFX v.0.1

---
+++ src/gr/aueb/cf/gui/MainWindowFX.java
@@ -0,0 +1,50 @@
+/*
+ * package gr.aueb.cf.gui;
+ * 
+ * import java.io.IOException; import javafx.fxml.FXMLLoader; import
+ * javafx.scene.Parent; import javafx.scene.Scene;
+ * 
+ * public class MainWindowFX { private static Scene scene;
+ * 
+ * public static Scene getMainWindow() throws IOException { Parent root =
+ * FXMLLoader.load(MainWindowFX.class.getResource("../view/MainWindowFX.fxml"));
+ * scene = new Scene(root, 500, 300); return scene; } }
+ */
+
+
+
+/*
+ * titleLbl = new Label("Quality in Education");
+ * titleLbl.setStyle("-fx-font-name:Arial; -fx-font-size:34;" +
+ * " -fx-font-weight:bold; -fx-text-fill:Maroon"); hb = new HBox();
+ * hb.setPadding(new Insets(20, 0, 0, 0)); hb.setAlignment(Pos.CENTER);
+ * hb.getChildren().add(titleLbl);
+ * 
+ * teachersBtn = new Button(""); teachersBtn.setMinWidth(50);
+ * teachersBtn.setMinHeight(50); 
+ * teachersBtn.setOnAction(e -> openTeachersMenu()); 
+ * GridPane.setConstraints(teachersBtn, 0, 0);
+ * 
+ * versionBtn = new Button(""); versionBtn.setMinWidth(50);
+ * versionBtn.setMinHeight(50); versionBtn.setOnAction(e -> openVersionForm());
+ * GridPane.setConstraints(versionBtn, 0, 1);
+ * 
+ * teachersLbl = new Label("Teachers");
+ * teachersLbl.setStyle("-fx-font-name:Arial; -fx-font-size:22;" +
+ * " -fx-font-weight:bold; -fx-text-fill:Blue");
+ * GridPane.setConstraints(teachersLbl, 1, 0);
+ * 
+ * versionLbl = new Label("Version");
+ * versionLbl.setStyle("-fx-font-name:Arial; -fx-font-size:22;" +
+ * " -fx-font-weight:bold; -fx-text-fill:Blue");
+ * GridPane.setConstraints(versionLbl, 1, 1);
+ * 
+ * gp = new GridPane(); gp.setPadding(new Insets(20, 10, 10, 20));
+ * gp.setHgap(10); gp.setVgap(15); gp.getChildren().addAll(teachersBtn,
+ * teachersLbl, versionBtn, versionLbl);
+ * 
+ * 
+ * vb = new VBox(10); vb.setAlignment(Pos.TOP_CENTER);
+ * vb.getChildren().addAll(hb, gp);
+ */	
+
